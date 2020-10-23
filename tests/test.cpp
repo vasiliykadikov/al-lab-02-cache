41392b38f332d9b0a0d5358958769daf3c5d1eb9
vasiliykadikov <61242428+vasiliykadikov@users.noreply.github.com>
2020-10-23 20:25:18 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,12 @@
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(CACHE, Test1) {
+    EXPECT_EQ(array_checks(16), 32);
+}
+TEST(CACHE, Test2) {
+    EXPECT_EQ(array_checks(576), 640);
+}
+TEST(CACHE, Test3) {
+    EXPECT_EQ(array_checks(1792), 2048);
 }
